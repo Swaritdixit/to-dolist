8627e752761454a52c7cfc450078995b4346cdb2
Swaritdixit <swaritdixit351@gmail.com>
2024-12-21 16:54:33 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,25 +1,53 @@
+document.addEventListener('DOMContentLoaded',()=>{
 let input=document.querySelector("#type");
 let button=document.querySelector("#button");
 let list=document.querySelector(".list");
-let c=0;
+
+const addTaskToDOM=(task,iscomplete=false)=>{
+   const listitem=document.createElement("li");
+   listitem.innerHTML=`${task}<button  style="margin-left:20px;" >Delete</button>`;
+   if(iscomplete)
+   {
+      listitem.style.textDecoration = 'line-through';
+
+   }
+   const deleteBtn=listitem.querySelector('button');
+   deleteBtn.addEventListener("click",()=>{
+   listitem.remove();
+   saveTasks();
+   });
+     list.appendChild(listitem);
+      
+      listitem.addEventListener("click",()=>{
+             if(listitem.style.textDecoration==='none')
+               listitem.style.textDecoration='line-through';
+            else
+            listitem.style.textDecoration='none';
+         saveTasks();
+      });
+     
+}
+const loadTasks=()=>
+{
+   const tasks=JSON.parse(localStorage.getItem('tasks'))||[];
+   tasks.forEach(task=>addTaskToDOM(task.text,task.iscomplete));
+
+};
+const saveTasks=()=>{
+   const tasks = Array.from(list.children).map(li => ({
+      text: li.firstChild.textContent.trim(),
+      iscomplete:li.style.textDecoration==='line-through'
+ } ));
+   localStorage.setItem('tasks',JSON.stringify(tasks));
+}
 button.addEventListener('click',()=>{ 
    const add=input.value.trim();
    if(add!=="")
-   {
-      const listitem=document.createElement("li");
-      listitem.innerHTML=`${add}<button  style="margin-left:20px;" >Delete</button>`;
-      const deleteBtn=listitem.querySelector('button');
-      deleteBtn.addEventListener("click",()=>{
-      listitem.remove();
-
-      });
-        list.appendChild(listitem);
+   {  addTaskToDOM(add); 
+         saveTasks();
          input.value="";
-         listitem.addEventListener("click",()=>{
-                if(listitem.style.textDecoration==='none')
-                  listitem.style.textDecoration='line-through';
-               else
-               listitem.style.textDecoration='none';
-         });
    }
+   
+});
+loadTasks();
 });
